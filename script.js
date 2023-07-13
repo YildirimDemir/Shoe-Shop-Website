51443b1bc6ecf3f0e49c6f67e04d02a53e857251
Yildirim Demir <yildirimdemir77@gmail.com>
2023-07-13 12:47:20 +0300

    Nav content

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+//! NAV
+
+//* NAV STICKY
+
+const nav = document.querySelector("nav");
+const forStickyNav = document.querySelector(".category");
+const initialCoord = forStickyNav.getClientRects();
+
+window.addEventListener("scroll", function() {
+  if (window.scrollY > initialCoord[0].top) {
+    nav.classList.add("nav-sticky");
+  } else {
+    nav.classList.remove("nav-sticky");
+  }
+});
+
+//* NAV BEFORE POPUP MESSAGE
+
+const message = document.createElement("div");
+message.classList.add("cookie-msg");
+
+setTimeout(function() {
+  message.innerHTML = 'Take advantage of 20% discount on purchases of €150 or more <button class="btn--close-cookie">X</button>';
+}, 2000);
+
+message.classList.add("cookie-msg");
+nav.append(message);
+nav.before(message);
+
+message.style.textAlign = "center";
+
+
+//!CATEGORY
+
+
+
+
+
+
+
+
+
+//! NEWSLATTER
 const modal = document.querySelector(".formModal");
 const overlay = document.querySelector(".overlay");
 const btnshowModal = document.querySelector(".showModal");
